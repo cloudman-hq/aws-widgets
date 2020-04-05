f3f838cafe7b7d922d19e19167e2f7d8030e73d0
PengXiao <eagle.xiao@gmail.com>
2020-04-05 10:27:55 +1000

    Removed nav links

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Settings from './components/Settings'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import './App.css';
 
@@ -14,16 +13,6 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/settings">Settings</Link>
-            </li>
-          </ul>
-        </nav>
         <Switch>
           <Route path="/settings">
             <Settings/>
