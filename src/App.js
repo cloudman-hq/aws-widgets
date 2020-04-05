3d59e11140c3e31658abc1fbdb4cffdf2a642307
PengXiao <eagle.xiao@gmail.com>
2020-04-05 10:09:28 +1000

    Replaced settings.html with react-router.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import EC2 from './components/EC2'
+import EC2 from './components/EC2';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <EC2/>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/settings">
+            <div>Here comes the settings</div>
+          </Route>
+          <Route path="/">
+            <EC2/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+);
 }
 
 export default App;
