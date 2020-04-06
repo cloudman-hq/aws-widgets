83fdc79219446acf85485182c8b7356631ffb630
zheng gong <zhenggong@192-168-1-101.tpgi.com.au>
2020-04-06 21:33:22 +1000

    feat(scaffold): create a new scaffold

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import './main';
+import App from './components/App';
+
+const render = (Main: React.FunctionComponent) => {
+  ReactDom.render(<Main />, document.querySelector('#app-root'));
+};
+
+render(App);
+
+if ((module as any).hot) {
+  (module as any).hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
+    render(NextApp);
+  });
+}
