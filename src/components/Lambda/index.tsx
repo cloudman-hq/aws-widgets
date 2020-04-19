236d3e3d0d2c1d680fab799379ad19f26b837112
Eric <gzcisco720@gmail.com>
2020-04-19 21:15:54 +1000

    feat(webpack): add optimization options

--- src/components/Lambda/index.tsx
+++ src/components/Lambda/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { LambdaCard } from './style';
-import '../../styles/style.css';
+
 
 class Lambda extends React.Component<any> {
   constructor(props: any) {
@@ -11,23 +11,23 @@ class Lambda extends React.Component<any> {
     return (
       <LambdaCard className="resource-root lambda">
         <div className="property" >
-          <label  className="field-name resource-logo">Name</label>
-          <span className="field-icon lambda"/>
+          <label className="field-name resource-logo">Name</label>
+          <span className="field-icon lambda" />
           <span className="field-value">{this.props.name}</span>
         </div>
         <div className="property" >
           <label className="field-name runtime">Runtime</label>
-          <span className="field-icon runtime"/>
+          <span className="field-icon runtime" />
           <span className="field-value">{this.props.runtime}</span>
         </div>
         <div className="property" >
-          <label  className="field-name role">Role</label>
-          <span className="icon role"/>
+          <label className="field-name role">Role</label>
+          <span className="icon role" />
           <span className="field-value">{this.props.role}</span>
         </div>
         <div className="property" >
-          <label  className="field-name role">Tags</label>
-          <span className="icon role"/>
+          <label className="field-name role">Tags</label>
+          <span className="icon role" />
           <span className="field-value">{this.props.tags}</span>
         </div>
       </LambdaCard>
