643037efa5072475b8f6cd9c6fba72f14a56091b
Ivy <ivy.lyu@myer.com.au>
2020-05-03 16:06:10 +1000

    ecs aws connection

--- src/components/ECS/index.tsx
+++ src/components/ECS/index.tsx
@@ -1,19 +1,111 @@
-import * as React from "react";
-import ECSCard from "./ECSCard";
-import ECSProperty from "./ECSProperty";
-import ECSLogo from "./amazon_ecs-icon.svg";
+import * as React from 'react';
+import ECSCard from './ECSCard';
+import ECSProperty from './ECSProperty';
+import ECSLogo from './amazon_ecs-icon.svg';
+import { inject, observer } from 'mobx-react';
+import { autorun } from 'mobx';
+import * as AWS from 'aws-sdk';
 
-class ECS extends React.Component<any> {
+interface State {
+  isLoading: boolean;
+  clusterName: string;
+  services: string;
+  tasks: string;
+}
+
+@inject(({ rootStore }) => ({
+  appStore: rootStore.getAppStore(),
+  settingStore: rootStore.getSettingStore(),
+}))
+@observer
+class ECS extends React.Component<any, State> {
   constructor(props: any) {
     super(props);
+    this.state = {
+      isLoading: true,
+      clusterName: '',
+      services: '',
+      tasks: '',
+    };
+    this.describe = this.describe.bind(this);
   }
 
+  componentDidMount(): void {
+    autorun(this.describe);
+  }
+
+  describe() {
+    if (
+      !this.props.settingsStore.isAccessSetup ||
+      !this.props.appStore.isRegionAndResourceSetup
+    ) {
+      return;
+    }
+
+    AWS.config.region = this.props.appStore.region;
+
+    AWS.config.credentials = this.props.settingsStore.awsCredentials;
+
+    const ecs = new AWS.ECS();
+
+    const params = {};
+    ecs.listClusters(params, function (err, data) {
+      if (!err) {
+        this.setState({
+          clusterName: data.clusterArns,
+        });
+      }
+      /*
+  data = {
+   clusterArns: [
+      "arn:aws:ecs:us-east-1:<aws_account_id>:cluster/test",
+      "arn:aws:ecs:us-east-1:<aws_account_id>:cluster/default"
+   ]
+  }
+  */
+    });
+
+    ecs.listServices(params, function (err, data) {
+      if (!err) {
+        this.setState({
+          services: data.serviceArns,
+        });
+      }
+      // an error occurred
+      /*
+      data = {
+       serviceArns: [
+          "arn:aws:ecs:us-east-1:012345678910:service/my-http-service"
+       ]
+      }
+      */
+    });
+
+    const params1 = {
+      cluster: 'test',
+    };
+    ecs.listTasks(params1, function (err, data) {
+      if (!err) {
+        this.setState({
+          tasks: data.taskArns,
+        });
+      }
+      /*
+       data = {
+        taskArns: [
+           "arn:aws:ecs:us-east-1:012345678910:task/0cc43cdb-3bee-4407-9c26-c0e6ea5bee84",
+           "arn:aws:ecs:us-east-1:012345678910:task/6b809ef6-c67e-4467-921f-ee261c15a0a1"
+        ]
+       }
+       */
+    });
+  }
   render() {
     return (
       <ECSCard title="ECS" icon={ECSLogo}>
-        <ECSProperty name={"clusterName"} value={this.props.clusterName} />
-        <ECSProperty name={"Serices"} value={this.props.serices} />
-        <ECSProperty name={"Tasks"} value={this.props.tasks} />
+        <ECSProperty name={'clusterName'} value={this.state.clusterName} />
+        <ECSProperty name={'Serices'} value={this.state.services} />
+        <ECSProperty name={'Tasks'} value={this.state.tasks} />
       </ECSCard>
     );
   }
--- src/components/Route/index.tsx
+++ src/components/Route/index.tsx
@@ -4,12 +4,16 @@ import EC2 from '../EC2';
 import Settings from '../Settings';
 import Editor from '../Editor';
 import Viewer from '../Viewer';
+import ECS from '../ECS';
 
-export default () => <>
-  <Route path="/" exact render={() => <Redirect to="/viewer" />} />
-  <Route path="/index.html" exact render={() => <Redirect to="/viewer" />} />
-  <Route path="/settings" component={Settings} />
-  <Route path="/editor" component={Editor} />
-  <Route path="/viewer" component={Viewer} />
-  <Route path="/ec2" component={EC2} />
-</>;
+export default () => (
+  <>
+    <Route path="/" exact render={() => <Redirect to="/viewer" />} />
+    <Route path="/index.html" exact render={() => <Redirect to="/viewer" />} />
+    <Route path="/settings" component={Settings} />
+    <Route path="/editor" component={Editor} />
+    <Route path="/viewer" component={Viewer} />
+    <Route path="/ec2" component={EC2} />
+    <Route path="/ecs" component={ECS} />
+  </>
+);
