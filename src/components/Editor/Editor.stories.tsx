b0b50fc5403fbde81bbe1862b57ab913b6b37f44
mwpeng <mingwei.peng.tech@gmail.com>
2020-05-09 16:37:13 +1000

    use different root store for stories

--- src/components/Editor/Editor.stories.tsx
+++ src/components/Editor/Editor.stories.tsx
@@ -8,18 +8,14 @@ export default {
     component: Editor
 }
 
-function newRootStore() {
-    return new RootStore();
-}
-
 export const WithoutAccessKeyAndSecret = () =>
-    <Provider rootStore={newRootStore()}>
+    <Provider rootStore={new RootStore()}>
         <Editor/>
     </Provider>;
 
 
 export const WithAccessKeyAndSecret = () => {
-    let rootStore = newRootStore();
+    let rootStore = new RootStore();
     rootStore.getSettingsStore().setAccessKey("key")
     rootStore.getSettingsStore().setSecretKey("secret")
     return <Provider rootStore={rootStore}>
