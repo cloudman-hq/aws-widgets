1e06bcc734c9f58cc9ae49913007e9268471e822
mwpeng <mingwei.peng.tech@gmail.com>
2020-05-11 17:15:22 +1000

    add mock aws and test

--- src/components/Viewer/Resources/S3/S3Service.tsx
+++ src/components/Viewer/Resources/S3/S3Service.tsx
@@ -1,17 +1,16 @@
-import * as AWS from 'aws-sdk';
+import { S3 } from 'aws-sdk';
 
 type BucketName = string;
 
-const s3 = new AWS.S3();
-
 export async function s3GetIsPublic(bucketName: BucketName): Promise<string> {
+  const s3 = new S3();
   return s3.getBucketPolicyStatus({ Bucket: bucketName })
-      .promise()
-      .then(
-          (value) => {
-            return value.PolicyStatus.IsPublic.toString();
-          },
-          (error: AWS.AWSError) => {
-            return `Property can not be loaded:${error.message}`;
-          });
+    .promise()
+    .then(
+      (value) => {
+        return value.PolicyStatus.IsPublic.toString();
+      },
+      (error: AWS.AWSError) => {
+        return `Property can not be loaded:${error.message}`;
+      });
 }
