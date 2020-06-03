9f92e1bfbbea93f982369cd391811bca8002648a
PengXiao <eagle.xiao@gmail.com>
2020-06-03 20:43:02 +1000

    Update spreadsheet for install and uninstall.

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,31 @@
 const functions = require('firebase-functions');
 const descriptor = require('./atlassian-connect.json');
+const SteinStore = require('stein-js-client');
+const store = new SteinStore('https://api.steinhq.com/v1/storages/5ed5fe9883c30d0425e2c433');
 
 exports.installedEndpoint = functions.https.onRequest((request, response) => {
   console.log('request.body.baseUrl:', request.body.baseUrl);
-  console.log('request.body.clientKey:', request.body.clientKey);
-  console.log('request.body.pluginsVersion:', request.body.pluginsVersion);
+  store.append('AWSWidgets', [
+    {
+      DateTime: new Date().toLocaleString('en-AU'),
+      ClientSite: request.body.baseUrl,
+      EventType: 'Install',
+      Notes: ''
+    }
+  ]).then(console.log)
   response.status(200).send(`OK`);
 });
 
 exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
   console.log('request.body.baseUrl:', request.body.baseUrl);
-  console.log('request.body.clientKey:', request.body.clientKey);
-  console.log('request.body.pluginsVersion:', request.body.pluginsVersion);
+  store.append('Sheet1', [
+    {
+      DateTime: new Date().toLocaleString('en-AU'),
+      ClientSite: request.body.baseUrl,
+      EventType: 'Uninstall',
+      Notes: ''
+    }
+  ]).then(console.log)
   response.status(200).send(`OK`);
 });
 
@@ -22,20 +36,6 @@ exports.descriptor = functions.https.onRequest((req, resp) => {
   descriptor.baseUrl = `${req.protocol}://${req.hostname}${basePath}`;
   // This is not necessary but works as a defense.
   descriptor.links.self = self;
-
-  const isLite = url.includes('lite');
-  if (isLite) {
-    descriptor.key = 'com.zenuml.confluence-addon-lite';
-    descriptor.name = 'ZenUML Lite';
-    descriptor.description = 'ZenUML Lite add-on';
-    descriptor.enableLicensing = false;
-    descriptor.modules.dynamicContentMacros.forEach(macro => {
-      if (macro.key === 'zenuml-sequence-macro') {
-        macro.key = 'zenuml-sequence-macro-lite';
-        macro.name.value = 'ZenUML Lite';
-      }
-    })
-  }
 
   resp.json(descriptor);
 })
--- src/components/Demo.stories.tsx
+++ src/components/Demo.stories.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import { State, Store } from '@sambego/storybook-state';
 import EC2 from '../components/Viewer/Resources/EC2'
 import Editor from '../components/Editor'
-// import '../styles/app.css'
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'mobx-react';
 import RootStore from '../RootStore';
 
-import App from "./App";
 
 export default {
   title: 'Demo',
@@ -16,17 +12,10 @@ export default {
 
 let rootStore = new RootStore();
 
-const store = new Store({
-  accessKey: '',
-  secretKey: ''
-});
 function handleInputChange(event: any) {
   const value = event.target.value;
 
   const name = event.target.name;
-  store.set({
-    [name]: value
-  });
 
   if (name === 'accessKey') {
     rootStore.getSettingsStore().setAccessKey(value)
@@ -36,11 +25,6 @@ function handleInputChange(event: any) {
   }
 }
 
-export const EC2Component = () => (
-  <div>
-    <EC2/>
-  </div>
-);
 export const EditorComponent = () => (
   <Provider rootStore={rootStore}>
   <div
@@ -70,41 +54,10 @@ export const EditorComponent = () => (
         </div>
       </form>
 
-        <State store={store}>
-          <Editor accessKey={store.state.accessKey} secretKey={store.state.secretKey} />
-        </State>
+      <Editor  />
     </div>
 
   </div>
   </Provider>
 );
 
-export const AppStory = () => (
-  <Provider rootStore={new RootStore()}>
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Access Key:
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            name="accessKey"
-            type="string"
-            onChange={handleInputChange} />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Secret Key:
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            name="secretKey"
-            type="string"
-            onChange={handleInputChange} />
-        </label>
-      </div>
-    </form>
-    <Router>
-      <App location="/editor" />
-    </Router>
-  </Provider>
-);
