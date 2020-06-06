bc44528fe382f8b378fc515fd6bfa1754e22bdd1
mwpeng <mingwei.peng.tech@gmail.com>
2020-06-06 11:33:35 +1000

    move resource type to resource module

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -7,22 +7,7 @@ import { autorun } from 'mobx';
 import { ErrorMessage, HelperMessage } from '@atlaskit/form';
 import DefaultCard from './DefaultCard';
 import * as AWS from 'aws-sdk';
-
-/**
- * Include the following scenarios:
- * 1. Access not setup - ACCESS_NOT_SETUP
- * 2. Access keys not valid - ACCESS_NOT_VALID
- * 3. Resource ID not provided - RESOURCE_ID_NOT_PROVIDED
- * 4. Resource does not exist - RESOURCE_DOES_NOT_EXIST
- * then all resources
- */
-enum ResourceType {
-  UNKNOWN,
-  INITIALISING,
-  LAMBDA_FUNCTION,
-  EC2,
-  Generic,
-}
+import { ResourceType } from './Resources';
 
 interface State {
   resourceType: ResourceType;
