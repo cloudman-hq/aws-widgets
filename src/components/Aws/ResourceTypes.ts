e38e5488bebcc9053a4948f647d4b3da8d73572c
Ivy <ivy.lyu@myer.com.au>
2020-06-14 19:25:13 +1000

    WIP ecs

--- src/components/Aws/ResourceTypes.ts
+++ src/components/Aws/ResourceTypes.ts
@@ -1,30 +1,34 @@
-import * as AWS from 'aws-sdk';
-import ec2logo from './icons/ec2.svg';
-import ecslogo from './icons/ecs.svg';
-import dynamodblogo from './icons/dynamodb.svg';
-import s3logo from './icons/s3.svg';
-import lambdalogo from './icons/lambda.svg';
+import * as AWS from "aws-sdk";
+import ec2logo from "./icons/ec2.svg";
+import ecslogo from "./icons/ecs.svg";
+import dynamodblogo from "./icons/dynamodb.svg";
+import s3logo from "./icons/s3.svg";
+import lambdalogo from "./icons/lambda.svg";
 
-const s3CorsLink = 'https://docs.aws.amazon.com/AmazonS3/latest/dev/cors.html';
+const s3CorsLink = "https://docs.aws.amazon.com/AmazonS3/latest/dev/cors.html";
 
 function emptyIfRejected<T>(p: Promise<T>) {
   return new Promise<T>((resolv, reject) =>
-    p.then(d => resolv(d), (e) => {
-      if (e.toString().includes('NetworkingError')) {
-        reject(e);
-      } else {
-        resolv(null);
+    p.then(
+      (d) => resolv(d),
+      (e) => {
+        if (e.toString().includes("NetworkingError")) {
+          reject(e);
+        } else {
+          resolv(null);
+        }
       }
-    }));
+    )
+  );
 }
 
-const resourceTypes =
-  [
-    {
-      name: 'EC2',
-      keywordInResourceId: 'i-',
-      icon: ec2logo,
-      list: (region: string, credentials: any) => new Promise((resolv, reject) => {
+const resourceTypes = [
+  {
+    name: "EC2",
+    keywordInResourceId: "i-",
+    icon: ec2logo,
+    list: (region: string, credentials: any) =>
+      new Promise((resolv, reject) => {
         AWS.config.credentials = credentials;
         AWS.config.region = region;
         const ec2 = new AWS.EC2();
@@ -34,12 +38,17 @@ const resourceTypes =
             reject(err);
           } else {
             const instances = data.Reservations.flatMap((r: any) =>
-              r.Instances.map((i: any) => ({ label: i.PrivateIpAddress, value: i.InstanceId })));
+              r.Instances.map((i: any) => ({
+                label: i.PrivateIpAddress,
+                value: i.InstanceId,
+              }))
+            );
             resolv(instances);
           }
         });
       }),
-      properties: (resourceId: string) => new Promise((resolv, reject) => {
+    properties: (resourceId: string) =>
+      new Promise((resolv, reject) => {
         const params = { InstanceIds: [resourceId] };
         new AWS.EC2().describeInstances(params, (err: any, data: any) => {
           if (err) {
@@ -48,8 +57,10 @@ const resourceTypes =
             const convert = (instance: any) => {
               const instanceState = instance.State.Name;
               const availabilityZone = instance.Placement.AvailabilityZone;
-              const securityGroups = instance.SecurityGroups
-                && instance.SecurityGroups.map((g: any) => g.GroupName) || [];
+              const securityGroups =
+                (instance.SecurityGroups &&
+                  instance.SecurityGroups.map((g: any) => g.GroupName)) ||
+                [];
               return {
                 Name: resourceId,
                 State: instanceState,
@@ -57,24 +68,32 @@ const resourceTypes =
                 Storage: instance.RootDeviceType,
                 AZ: availabilityZone,
                 Key: instance.KeyName,
-                IAM: instance.IamInstanceProfile && instance.IamInstanceProfile.Arn,
-                SGs: securityGroups.join(', '),
+                IAM:
+                  instance.IamInstanceProfile &&
+                  instance.IamInstanceProfile.Arn,
+                SGs: securityGroups.join(", "),
                 PrivateIp: instance.PrivateIpAddress,
                 PublicDns: instance.PublicDnsName,
-                Tags: instance.Tags && instance.Tags.map((t: any) => `${t.Key}: ${t.Value}`),
+                Tags:
+                  instance.Tags &&
+                  instance.Tags.map((t: any) => `${t.Key}: ${t.Value}`),
               };
             };
-            resolv(data.Reservations.length &&
-              convert(data.Reservations[0].Instances[0]) || {});
+            resolv(
+              (data.Reservations.length &&
+                convert(data.Reservations[0].Instances[0])) ||
+                {}
+            );
           }
         });
       }),
-    },
-    {
-      name: 'Lambda',
-      keywordInResourceId: 'arn:aws:lambda',
-      icon: lambdalogo,
-      list: (region: string, credentials: any) => new Promise((resolv, reject) => {
+  },
+  {
+    name: "Lambda",
+    keywordInResourceId: "arn:aws:lambda",
+    icon: lambdalogo,
+    list: (region: string, credentials: any) =>
+      new Promise((resolv, reject) => {
         AWS.config.credentials = credentials;
         AWS.config.region = region;
         const lambda = new AWS.Lambda();
@@ -83,17 +102,23 @@ const resourceTypes =
           if (err) {
             reject(err);
           } else {
-            resolv(data.Functions.map((f: any) =>
-              ({ label: f.FunctionName, value: f.FunctionArn })));
+            resolv(
+              data.Functions.map((f: any) => ({
+                label: f.FunctionName,
+                value: f.FunctionArn,
+              }))
+            );
           }
         });
       }),
-      properties: (resourceId: string) => new Promise((resolv, reject) => resolv({})),
-    },
-    {
-      name: 'S3',
-      icon: s3logo,
-      properties: (resourceId: string) => new Promise(async (resolv, reject) => {
+    properties: (resourceId: string) =>
+      new Promise((resolv, reject) => resolv({})),
+  },
+  {
+    name: "S3",
+    icon: s3logo,
+    properties: (resourceId: string) =>
+      new Promise(async (resolv, reject) => {
         const s3 = new AWS.S3();
         const param = { Bucket: resourceId };
         try {
@@ -104,13 +129,14 @@ const resourceTypes =
           ]);
           resolv({
             Bucket: resourceId,
-            IsPublic: policy && policy.PolicyStatus?.IsPublic.toString() || 'false',
-            Encrypted: encryption && encryption
-              .ServerSideEncryptionConfiguration
-              .Rules[0]
-              .ApplyServerSideEncryptionByDefault
-              .SSEAlgorithm || 'false',
-            Tags: tagging && tagging.TagSet.map(t => `${t.Key}: ${t.Value}`),
+            IsPublic:
+              (policy && policy.PolicyStatus?.IsPublic.toString()) || "false",
+            Encrypted:
+              (encryption &&
+                encryption.ServerSideEncryptionConfiguration.Rules[0]
+                  .ApplyServerSideEncryptionByDefault.SSEAlgorithm) ||
+              "false",
+            Tags: tagging && tagging.TagSet.map((t) => `${t.Key}: ${t.Value}`),
           });
         } catch (err) {
           resolv({
@@ -119,12 +145,13 @@ const resourceTypes =
           });
         }
       }),
-    },
-    {
-      name: 'ECS',
-      keywordInResourceId: 'arn:aws:',
-      icon: ecslogo,
-      list: (region: string, credentials: any) => new Promise((resolv, reject) => {
+  },
+  {
+    name: "ECS",
+    keywordInResourceId: "arn:aws:",
+    icon: ecslogo,
+    list: (region: string, credentials: any) =>
+      new Promise((resolv, reject) => {
         AWS.config.credentials = credentials;
         AWS.config.region = region;
         new AWS.ECS().listClusters({}, (err: any, data: any) => {
@@ -132,64 +159,147 @@ const resourceTypes =
             reject(err);
           } else {
             const parseName = (arn: string) => {
-              const pos = arn.indexOf('/');
+              const pos = arn.indexOf("/");
               if (pos + 1 < arn.length) {
                 return arn.substring(pos + 1);
               }
               return arn;
             };
-            resolv(data.clusterArns.map((c: any) =>
-              ({ label: parseName(c), value: c })));
+            resolv(
+              data.clusterArns.map((c: any) => ({
+                label: parseName(c),
+                value: c,
+              }))
+            );
           }
         });
       }),
-      properties: (resourceId: string) => new Promise((resolv, reject) => {
-        new AWS.ECS().describeClusters({ clusters: [resourceId] }, (err, data) => {
+    properties: (resourceId: string) =>
+      new Promise(async (resolv, reject) => {
+        const ecs = new AWS.ECS();
+        const param = { clusters: [resourceId] };
+        const params = { cluster: resourceId };
+        try {
+          const [
+            { clusters },
+            { taskArns },
+            { serviceArns },
+          ] = await Promise.all([
+            emptyIfRejected(ecs.describeClusters(param).promise()),
+            emptyIfRejected(ecs.listTasks(params).promise()),
+            emptyIfRejected(ecs.listServices(params).promise()),
+          ]);
+          console.log(JSON.stringify(clusters));
+          resolv({
+            Name: clusters.length && clusters[0].clusterName,
+            Status: clusters.length && clusters[0].status,
+            RunningTasksCount: clusters.length && clusters[0].runningTasksCount,
+            PendingTasksCount: clusters.length && clusters[0].pendingTasksCount,
+            ActiveServicesCount:
+              clusters.length && clusters[0].activeServicesCount,
+            TaskArns: taskArns.length && taskArns.map((task) => task),
+            Services:
+              serviceArns.length && serviceArns.map((service) => service),
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }),
+  },
+  // ecs.describeClusters(, (err, data) => {
+  //   if (err) {
+  //     reject(err);
+  //   } else {
+  //     resolv(
+  //       (data.clusters.length && {
+  //         Name: data.clusters[0].clusterName,
+  //         Status: data.clusters[0].status,
+  //         RunningTasksCount: data.clusters[0].runningTasksCount,
+  //         PendingTasksCount: data.clusters[0].pendingTasksCount,
+  //         ActiveServicesCount: data.clusters[0].activeServicesCount,
+  //       }) ||
+  //         {}
+  //     );
+  //   }
+  // });
+  // ecs.listClusters({}, (err, data) => {
+  //   if (err) {
+  //     reject(err);
+  //   } else {
+  //     resolv(
+  //       (data.clusterArns.length && {
+  //         Name: "ds",
+  //       }) ||
+  //         {}
+  //     );
+  //   }
+  // });
+  // }),
+
+  {
+    name: "Dynamodb",
+    keywordInResourceId: "arn:aws:dynamodb",
+    icon: dynamodblogo,
+    list: (region: string, credentials: any) =>
+      new Promise((resolv, reject) => {
+        AWS.config.credentials = credentials;
+        AWS.config.region = region;
+        new AWS.DynamoDB().listTables({}, (err: any, data: any) => {
           if (err) {
             reject(err);
           } else {
-            resolv(data.clusters.length && {
-              Name: data.clusters[0].clusterName,
-              Status: data.clusters[0].status,
-            } || {});
+            resolv(
+              (data.clusters.length && {
+                Name: data.clusters[0].clusterName,
+                Status: data.clusters[0].status,
+              }) ||
+                {}
+            );
           }
         });
       }),
-    },
-    {
-      name: 'Dynamodb',
-      keywordInResourceId: 'arn:aws:dynamodb',
-      icon: dynamodblogo,
-      list: (region: string, credentials: any) =>
-        new Promise((resolv, reject) => {
-          AWS.config.credentials = credentials;
-          AWS.config.region = region;
-          new AWS.DynamoDB().listTables({}, (err: any, data: any) => {
+  },
+  {
+    name: "Dynamodb",
+    keywordInResourceId: "arn:aws:dynamodb",
+    icon: dynamodblogo,
+    list: (region: string, credentials: any) =>
+      new Promise((resolv, reject) => {
+        AWS.config.credentials = credentials;
+        AWS.config.region = region;
+        new AWS.DynamoDB().listTables({}, (err: any, data: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolv(data.TableNames.map((t: any) => ({ label: t, value: t })));
+          }
+        });
+      }),
+    properties: (resourceId: string) =>
+      new Promise((resolv, reject) => {
+        new AWS.DynamoDB().describeTable(
+          { TableName: resourceId },
+          (err, data) => {
             if (err) {
               reject(err);
             } else {
-              resolv(data.TableNames.map((t: any) =>
-                ({ label: t, value: t })));
+              resolv(
+                (data.Table && {
+                  Table: resourceId,
+                  Items: data.Table.ItemCount,
+                }) ||
+                  {}
+              );
             }
-          });
-        }),
-      properties: (resourceId: string) => new Promise((resolv, reject) => {
-        new AWS.DynamoDB().describeTable({ TableName: resourceId }, (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolv(data.Table && {
-              Table: resourceId,
-              Items: data.Table.ItemCount,
-            } || {});
           }
-        });
+        );
       }),
-    },
-  ];
+  },
+];
 
-export const findByName = (name: string) => resourceTypes.find(t => t.name === name);
+export const findByName = (name: string) =>
+  resourceTypes.find((t) => t.name === name);
 export const findByResourceId = (resourceId: string) =>
-  resourceTypes.find(t => resourceId.includes(t.keywordInResourceId));
+  resourceTypes.find((t) => resourceId.includes(t.keywordInResourceId));
 
 export default resourceTypes;
---
+++ src/components/ECS/index.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import ECSCard from './ECSCard';
+import ECSProperty from './ECSProperty';
+import ECSLogo from './amazon_ecs-icon.svg';
+import { autorun } from 'mobx';
+import * as AWS from 'aws-sdk';
+
+interface State {
+  isLoading: boolean;
+  clusterName: string;
+  services: string;
+  tasks: string;
+  az: string;
+}
+
+class ECS extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      clusterName: '',
+      services: '',
+      tasks: '',
+      az: '',
+    };
+    this.describe = this.describe.bind(this);
+  }
+
+  componentDidMount(): void {
+    autorun(this.describe);
+  }
+
+  describe() {
+    this.setState({
+      isLoading: true,
+    });
+
+    const ecs = new AWS.ECS();
+    const params = {};
+
+    ecs.listClusters(params, (err, data) => {
+      if (!err) {
+        this.setState({
+          clusterName: data.clusterArns[0].split('/')[1],
+          isLoading: false,
+        });
+      }
+      /*
+  data = {
+   clusterArns: [
+      "arn:aws:ecs:us-east-1:<aws_account_id>:cluster/test",
+      "arn:aws:ecs:us-east-1:<aws_account_id>:cluster/default"
+   ]
+  }
+  */
+    });
+
+    ecs.listServices(params, (err, data) => {
+      if (!err) {
+        this.setState({
+          services: data.serviceArns[0],
+        });
+      }
+      // an error occurred
+      /*
+      data = {
+       serviceArns: [
+          "arn:aws:ecs:us-east-1:012345678910:service/my-http-service"
+       ]
+      }
+      */
+    });
+
+    const params1 = {
+      cluster: 'test',
+    };
+    ecs.listTasks(params1, (err, data) => {
+      if (!err) {
+        this.setState({
+          tasks: data.taskArns[0],
+        });
+      }
+      /*
+       data = {
+        taskArns: [
+           "arn:aws:ecs:us-east-1:012345678910:task/0cc43cdb-3bee-4407-9c26-c0e6ea5bee84",
+           "arn:aws:ecs:us-east-1:012345678910:task/6b809ef6-c67e-4467-921f-ee261c15a0a1"
+        ]
+       }
+       */
+    });
+  }
+  render() {
+    return (
+      <ECSCard title="ECS" icon={ECSLogo}>
+        <ECSProperty name={'clusterName'} value={this.state.clusterName} />
+        <ECSProperty name={'Serices'} value={this.state.services} />
+        <ECSProperty name={'Tasks'} value={this.state.tasks} />
+      </ECSCard>
+    );
+  }
+}
+
+export default ECS;
