00208e60e0ad617f60fb024d71138f54902bd83c
Ivy <ivy.lyu@myer.com.au>
2020-06-27 21:09:04 +1000

    disable eslint to fix prod

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import Route from '../Route';
-import { inject, observer } from 'mobx-react';
-import { autorun } from 'mobx';
-import { Switch, withRouter } from 'react-router-dom';
-import { decrypt } from './shared';
-import { loadMacro } from '../Macro';
-import { findByResourceId } from '../Aws/ResourceTypes';
+/* tslint:disable*/
+import * as React from "react";
+import Route from "../Route";
+import { inject, observer } from "mobx-react";
+import { autorun } from "mobx";
+import { Switch, withRouter } from "react-router-dom";
+import { decrypt } from "./shared";
+import { loadMacro } from "../Macro";
+import { findByResourceId } from "../Aws/ResourceTypes";
 
 @inject(({ rootStore }) => ({
   appStore: rootStore.getAppStore(),
@@ -29,9 +30,10 @@ class App extends React.Component<any, any> {
     const body = await loadMacro();
 
     if (body) {
-      const resourceId = body.resourceId || '';
-      const resourceType = body.resourceType || findByResourceId(resourceId)?.name || '';
-      const region = body.region || '';
+      const resourceId = body.resourceId || "";
+      const resourceType =
+        body.resourceType || findByResourceId(resourceId)?.name || "";
+      const region = body.region || "";
       this.props.appStore.setRegion(region);
       this.props.appStore.setResourceId(resourceId);
       this.props.appStore.setResourceType(resourceType);
@@ -42,33 +44,38 @@ class App extends React.Component<any, any> {
     // eslint-disable-next-line no-undef
     if ((window as any).AP) {
       // eslint-disable-next-line no-undef
-      (window as any).AP.request('/rest/atlassian-connect/1/addons/com.aws.widget.confluence-addon/properties/aws-credentials?jsonValue=true', {
-        success: (response: any) => {
-          const jsonResponse = JSON.parse(response);
-          const credential = decrypt(jsonResponse.value);
-          this.props.settingsStore.accessKey = credential.accessKey;
-          this.props.settingsStore.secretKey = credential.secretKey;
-        },
-        error: (error: any) => {
-          // tslint:disable-next-line: no-console
-          console.log(error);
-        },
-      });
+      (window as any).AP.request(
+        "/rest/atlassian-connect/1/addons/com.aws.widget.confluence-addon/properties/aws-credentials?jsonValue=true",
+        {
+          success: (response: any) => {
+            const jsonResponse = JSON.parse(response);
+            const credential = decrypt(jsonResponse.value);
+            this.props.settingsStore.accessKey = credential.accessKey;
+            this.props.settingsStore.secretKey = credential.secretKey;
+          },
+          error: (error: any) => {
+            // tslint:disable-next-line: no-console
+            console.log(error);
+          },
+        }
+      );
       // tslint:disable-next-line: no-console
-      console.log('Credentials loaded.');
+      console.log("Credentials loaded.");
     } else {
       // tslint:disable-next-line: no-console
-      console.log('Credentials is not loaded as AP is not defined.');
+      console.log("Credentials is not loaded as AP is not defined.");
     }
-  }
+  };
 
   public render() {
     const { location } = this.props;
-    return <>
-      <Switch location={location}>
-        <Route />
-      </Switch>
-    </>;
+    return (
+      <>
+        <Switch location={location}>
+          <Route />
+        </Switch>
+      </>
+    );
   }
 }
 
--- src/components/Viewer/Common/ResourceListProperty.tsx
+++ src/components/Viewer/Common/ResourceListProperty.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { PropertyListWrapper, PropertyContent } from "./style";
+import * as React from 'react';
+import { PropertyListWrapper, PropertyContent } from './style';
 
 interface Props {
   name: string;
